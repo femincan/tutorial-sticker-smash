36ed8e47a19c892c619f0477b9c689998d32ff83
Furkan Emin Can <femincanx@gmail.com>
2024-08-26 10:29:14 +0300

    feat: add image and emoji select feature

--- App.tsx
+++ App.tsx
@@ -1,11 +1,80 @@
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+import IconButton from './components/IconButton';
+import CircleButton from './components/CircleButton';
+import EmojiPicker from './components/EmojiPicker';
+import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState<string>();
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState<number>();
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={require('./assets/images/background-image.png')}
+          selectedImage={selectedImage}
+        />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
+      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon='refresh' onPress={() => setShowAppOptions(false)}>
+              Reset
+            </IconButton>
+            <CircleButton onPress={() => setIsModalVisible(true)} />
+            <IconButton icon='save-alt' onPress={() => {}}>
+              Save
+            </IconButton>
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme='primary' onPress={pickImage}>
+            Choose a photo
+          </Button>
+          <Button onPress={() => setShowAppOptions(true)}>
+            Use this photo
+          </Button>
+        </View>
+      )}
+      <EmojiPicker
+        isVisible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+      >
+        <EmojiList
+          onSelect={(item) => {
+            setPickedEmoji(item);
+          }}
+          onCloseModal={() => setIsModalVisible(false)}
+        />
+      </EmojiPicker>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +82,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
